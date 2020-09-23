580fbfc653368074232a925fca7b1bfe173b9553
ratrap44 <62080086+ratrap44@users.noreply.github.com>
2020-09-23 12:08:16 -0400

    lab04

---
+++ Aggp231Lab4/Assets/Scripts/ResetScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetScript : MonoBehaviour
+{
+    Vector3 OriginalPos;
+    public GameObject player;
+    public JoystickPlayerExample playercon;
+    // Start is called before the first frame update
+    void Start()
+    {
+        OriginalPos = player.gameObject.transform.position;
+    }
+
+    void Update()
+    {
+        playercon.speed = 1;
+    }
+
+    public void ResBut()
+    {
+        player.gameObject.transform.position = OriginalPos;
+        playercon.speed = 0;
+        
+    }
+}
